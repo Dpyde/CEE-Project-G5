a4b82b40a96bd8f1d6a71bf6b96089ea569c52e7
nattharat-naja <nattharatkl@gmail.com>
2024-04-18 01:09:07 +0700

    rePush HomePage

--- Project File/frontend/public/scripts/api.js
+++ Project File/frontend/public/scripts/api.js
@@ -23,54 +23,20 @@ export async function deleteItem(id, item) {
 }
 
 export async function filterItems(filterName, lowerPrice, upperPrice) {
-  try {
-    let data = {
-      filterName: filterName,
-      lowerPrice: lowerPrice,
-      upperPrice: upperPrice,
-    };
-
-    // Make a POST request to the backend with the data in the body
-    const response = await fetch(`${BACKEND_URL}/items/filter`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok.");
-    }
-
-    // Parse the response as JSON
-    const filteredItems = await response.json();
-
-    // Return the filtered items
-    return filteredItems;
-  } catch (error) {
-    console.error("Error fetching filtered items:", error);
-    return [];
-  }
+  // TODO3: implement this function
+  // You may need to understand handleFilterItem() function in ./table.js before implementing this function.
+  return /* return the filted items */;
 }
 
 export async function getMembers() {
-  const members = await fetch(`${BACKEND_URL}/members`).then((r) => r.json());
-  return members;
+  // TODO4: implement this function
+  return /* return all members */;
 }
 
 export async function createMember(member) {
-  await fetch(`${BACKEND_URL}/members`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(member),
-  });
+  // TODO4: implement this function
 }
 
 export async function deleteMember(id, item) {
-  await fetch(`${BACKEND_URL}/members/${id}`, {
-    method: "DELETE",
-  });
+  // TODO4: implement this function
 }
--- Project File/frontend/public/scripts/config.js
+++ Project File/frontend/public/scripts/config.js
@@ -1,10 +1,10 @@
-// TODO: change the BACKEND_URL to your EC2 public IPv4
-export const BACKEND_URL = "http://18.209.223.6:3222";
+// TODO0: change the BACKEND_URL to your EC2 public IPv4
+export const BACKEND_URL = "http://localhost:3222";
 
-// TODO: change element in MEMBERS to your group members.
+// TODO0: change element in MEMBERS to your group members.
 // WARNING: the _id parameters is required.
 export const MEMBERS = [
-  { name: "Ratchanon Thanatsang", _id: 1 },
-  { name: "Nantapong Ruangsuksriwong", _id: 2 },
-  { name: "Thukdanai Thaothawin", _id: 3 },
+  { name: "อติวงศ์ สุชาโต", _id: 1 },
+  { name: "โปรดปราน บุณยพุกกณะ", _id: 2 },
+  { name: "ณัฐวุฒิ หนูไฟโรจน์", _id: 3 },
 ];
