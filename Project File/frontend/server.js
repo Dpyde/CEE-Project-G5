a1168edf44dc2d420aaf32d562728b05ca51e0e3
nattharat-naja <nattharatkl@gmail.com>
2024-04-23 22:04:03 +0700

    prepare for deployment

--- Project File/frontend/server.js
+++ Project File/frontend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
+import { FRONTEND_PORT, FRONTEND_URL } from "./public/scripts/config.js";
 
 const app = express();
 
 app.use(express.static("public"));
 
-const PORT = 3221;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Frontend Server ready at http://localhost:${PORT}`);
+app.listen(FRONTEND_PORT, "0.0.0.0", () => {
+  console.log(`Frontend Server ready at ${FRONTEND_URL}`);
 });
